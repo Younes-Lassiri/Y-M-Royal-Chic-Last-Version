d46492613f222e6904d68dfd08ba960bc36fe6e2
Younes <lassiriyounesse04@gmail.com>
2024-02-07 22:36:17 +0100

    uuuu

--- server.js
+++ server.js
@@ -252,5 +252,5 @@ app.get('/api/messages', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on ${PORT}`);
 });
